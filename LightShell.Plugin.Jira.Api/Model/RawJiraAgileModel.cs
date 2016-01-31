9f338211108da84ed704f53e49d077aa1f086ef3
Adam Marszalek <adam.marszalek@gmail.com>
2016-01-31 16:53:19 +0100

    Fixed compilation error in old UI

---
+++ LightShell.Plugin.Jira.Api/Model/RawJiraAgileModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LightShell.Plugin.Jira.Api.Model
+{
+#pragma warning disable JustCode_CSharp_TypeFileNameMismatch // Types not matching file names
+   public class RawAgileBoardsList
+   {
+      public int MaxResults { get; set; }
+      public int StartAt { get; set; }
+      public bool IsLast { get; set; }
+      public RawAgileBoard[] Values { get; set; }
+   }
+
+   public class RawAgileBoard
+   {
+      public int Id { get; set; }
+      public string Self { get; set; }
+      public string Name { get; set; }
+      public string Type { get; set; }
+   }
+
+
+   public class RawAgileSprintsList
+   {
+      public int MaxResults { get; set; }
+      public int StartAt { get; set; }
+      public bool IsLast { get; set; }
+      public RawAgileSprint[] Values { get; set; }
+   }
+
+   public class RawAgileSprint
+   {
+      public int Id { get; set; }
+      public string Self { get; set; }
+      public string State { get; set; }
+      public string Name { get; set; }
+      public DateTime StartDate { get; set; }
+      public DateTime EndDate { get; set; }
+      public DateTime CompleteDate { get; set; }
+      public int OriginBoardId { get; set; }
+   }
+#pragma warning restore JustCode_CSharp_TypeFileNameMismatch // Types not matching file names
+}
