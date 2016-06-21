fa903d79fb4af257c46b5c5375f55a5fa64b4be3
Adam Marszalek <adam.marszalek@gmail.com>
2016-06-21 23:05:04 +0200

    Updated version

--- JiraAssistant/Properties/AssemblyInfo.cs
+++ JiraAssistant/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly 
 )]
 
-[assembly: AssemblyVersion("0.7.6.0")]
-[assembly: AssemblyFileVersion("0.7.6.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
