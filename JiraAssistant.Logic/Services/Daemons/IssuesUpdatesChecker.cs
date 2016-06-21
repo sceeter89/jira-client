ae5baf57458b9de6497e95daf4abb6d55cfd63eb
Adam Marszalek <adam.marszalek@gmail.com>
2016-06-21 19:55:59 +0200

    Added import of issues history, added option to skip changes made by logged in user. Closes #30. Improved update message to display what precisely changed. Closes #29.

--- JiraAssistant.Logic/Services/Daemons/IssuesUpdatesChecker.cs
+++ JiraAssistant.Logic/Services/Daemons/IssuesUpdatesChecker.cs
@@ -11,107 +11,133 @@ using System.Windows.Media;
 using System.Windows.Threading;
 using Telerik.Windows.Controls;
 using JiraAssistant.Logic.ContextlessViewModels;
+using System.Collections.Generic;
+using System.Text;
 
 namespace JiraAssistant.Logic.Services.Daemons
 {
     public class IssuesUpdatesChecker
-   {
-      private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
-      private readonly IJiraApi _jiraApi;
-      private readonly ReportsSettings _reportsSettings;
-      private readonly DispatcherTimer _timer;
-      private readonly ImageSourceConverter _imageSourceConverter = new ImageSourceConverter();
-      private readonly JiraSessionViewModel _jiraSession;
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private readonly IJiraApi _jiraApi;
+        private readonly ReportsSettings _reportsSettings;
+        private readonly DispatcherTimer _timer;
+        private readonly ImageSourceConverter _imageSourceConverter = new ImageSourceConverter();
+        private readonly JiraSessionViewModel _jiraSession;
 
-      public IssuesUpdatesChecker(ReportsSettings reportsSettings, IJiraApi jiraApi, JiraSessionViewModel jiraSession)
-      {
-         _reportsSettings = reportsSettings;
-         _jiraApi = jiraApi;
-         _jiraSession = jiraSession;
+        public IssuesUpdatesChecker(ReportsSettings reportsSettings, IJiraApi jiraApi, JiraSessionViewModel jiraSession)
+        {
+            _reportsSettings = reportsSettings;
+            _jiraApi = jiraApi;
+            _jiraSession = jiraSession;
 
-         _timer = new DispatcherTimer();
-         _timer.Interval = TimeSpan.FromSeconds(30);
-         _timer.Tick += TimerTick;
-         _timer.Start();
-      }
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(30);
+            _timer.Tick += TimerTick;
+            _timer.Start();
+        }
 
-      private void TimerTick(object sender, EventArgs e)
-      {
-         if (_jiraSession.IsLoggedIn == false)
-            return;
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (_jiraSession.IsLoggedIn == false)
+                return;
 
-         if (_reportsSettings.MonitorIssuesUpdates == false)
-            return;
+            if (_reportsSettings.MonitorIssuesUpdates == false)
+                return;
 
-         if (DateTime.Now - _reportsSettings.LastUpdatesScan > _reportsSettings.ScanForUpdatesInterval)
-            ScanForUpdates();
-      }
+            if (DateTime.Now - _reportsSettings.LastUpdatesScan > _reportsSettings.ScanForUpdatesInterval)
+                ScanForUpdates();
+        }
 
-      private DateTime GreatestDateTime(params DateTime[] dateTimes)
-      {
-         return dateTimes.Max();
-      }
+        private DateTime GreatestDateTime(params DateTime[] dateTimes)
+        {
+            return dateTimes.Max();
+        }
 
-      private async void ScanForUpdates()
-      {
-         var alertManager = new RadDesktopAlertManager();
+        private async void ScanForUpdates()
+        {
+            var alertManager = new RadDesktopAlertManager();
 
-         var changesSince = GreatestDateTime((DateTime.Now - TimeSpan.FromHours(24)), _reportsSettings.LastUpdatesScan);
-         var projectKeys = _reportsSettings.ProjectsList.Split(',').Select(p => p.Trim());
-         try
-         {
-            var query = string.Format("updated >= '{0}'", changesSince.ToString("yyyy-MM-dd HH:mm"));
-            if (projectKeys.Any())
-               query += string.Format(" AND project IN ({0})", string.Join(",", projectKeys));
-
-            var updatedIssues = await _jiraApi.SearchForIssues(query);
-            foreach (var issue in updatedIssues.Where(i => i.BuiltInFields.Updated >= _reportsSettings.LastUpdatesScan))
+            var changesSince = GreatestDateTime((DateTime.Now - TimeSpan.FromHours(24)), _reportsSettings.LastUpdatesScan);
+            var projectKeys = _reportsSettings.ProjectsList.Split(',').Select(p => p.Trim());
+            try
             {
-               var openIssueCommand = new RelayCommand(() =>
-               {
-                  var i = issue;
-                  Process.Start(string.Format("{0}/browse/{1}", _jiraApi.Server.ServerUri, i.Key));
-               });
+                var query = string.Format("updated >= '{0}'", changesSince.ToString("yyyy-MM-dd HH:mm"));
+                if (projectKeys.Any())
+                    query += string.Format(" AND project IN ({0})", string.Join(",", projectKeys));
 
-               if (issue.Created >= changesSince && _reportsSettings.ShowCreatedIssues)
-                  alertManager.ShowAlert(new RadDesktopAlert
-                  {
-                     Header = "New issue: " + issue.Key,
-                     Content = issue.Summary,
-                     ShowDuration = 5000,
-                     Icon = new Image { Source = GetImageFromResources("/Assets/Avatars/PlusSign.png"), Width = 48, Height = 48 },
-                     IconColumnWidth = 48,
-                     Command = openIssueCommand
-                  });
-               else if(issue.Created < changesSince && _reportsSettings.ShowUpdatedIssues)
-                  alertManager.ShowAlert(new RadDesktopAlert
-                  {
-                     Header = "Updated: " + issue.Key,
-                     Content = issue.Summary,
-                     ShowDuration = 5000,
-                     Icon = new Image { Source = GetImageFromResources("/Assets/Avatars/EditSign.png"), Width = 48, Height = 48 },
-                     IconColumnWidth = 48,
-                     Command = openIssueCommand
-                  });
+                var updatedIssues = await _jiraApi.SearchForIssues(query);
+                foreach (var issue in updatedIssues.Where(i => i.BuiltInFields.Updated >= _reportsSettings.LastUpdatesScan))
+                {
+                    var openIssueCommand = new RelayCommand(() =>
+                    {
+                        var i = issue;
+                        Process.Start(string.Format("{0}/browse/{1}", _jiraApi.Server.ServerUri, i.Key));
+                    });
+
+                    if (issue.Created >= changesSince && _reportsSettings.ShowCreatedIssues)
+                    {
+                        if (_reportsSettings.SkipOwnChanges && issue.BuiltInFields.Reporter.Name == _jiraSession.Profile.Name)
+                            continue;
+
+                        alertManager.ShowAlert(new RadDesktopAlert
+                        {
+                            Header = string.Format("New issue: {0}", issue.Key),
+                            Content = issue.Summary,
+                            ShowDuration = 5000,
+                            Icon = new Image { Source = GetImageFromResources("/Assets/Avatars/PlusSign.png"), Width = 48, Height = 48 },
+                            IconColumnWidth = 48,
+                            Command = openIssueCommand
+                        });
+                    }
+                    else if (issue.Created < changesSince && _reportsSettings.ShowUpdatedIssues)
+                    {
+                        var changes = issue.Changelog.Where(ch => ch.Created >= changesSince && !(_reportsSettings.SkipOwnChanges && issue.BuiltInFields.Reporter.Name == _jiraSession.Profile.Name));
+
+                        if (changes.Any() == false)
+                            continue;
+
+                        var rawChangeSummary = new Dictionary<string, string>();
+
+                        foreach (var change in changes)
+                            foreach (var item in change.Items)
+                            {
+                                rawChangeSummary[item.Field] = item.toString;
+                            }
+
+                        var changeSummaryBuilder = new StringBuilder();
+                        foreach (var changeSummary in rawChangeSummary)
+                            changeSummaryBuilder.AppendFormat("{0}: {1}\n", changeSummary.Key, changeSummary.Value.Substring(0, Math.Min(15, changeSummary.Value.Length)));
+
+                        alertManager.ShowAlert(new RadDesktopAlert
+                        {
+                            Header = string.Format("[{0}] {1}", issue.Key, issue.Summary.Substring(0, Math.Min(20, issue.Summary.Length))),
+                            Content = changeSummaryBuilder.ToString(),
+                            ShowDuration = 5000,
+                            Icon = new Image { Source = GetImageFromResources("/Assets/Avatars/EditSign.png"), Width = 48, Height = 48 },
+                            IconColumnWidth = 48,
+                            Command = openIssueCommand
+                        });
+                    }
+                }
+                _reportsSettings.LastUpdatesScan = DateTime.Now;
             }
-            _reportsSettings.LastUpdatesScan = DateTime.Now;
-         }
-         catch (SearchFailedException e)
-         {
-            _logger.Log(LogLevel.Warn, "Failed to retrieve updated issues.", e);
-            alertManager.ShowAlert(new RadDesktopAlert
+            catch (SearchFailedException e)
             {
-               Header = "Invalid configuration",
-               Content = "Please verify that you provided correct list of project keys",
-               ShowDuration = 2000
-            });
-         }
-      }
+                _logger.Log(LogLevel.Warn, "Failed to retrieve updated issues.", e);
+                alertManager.ShowAlert(new RadDesktopAlert
+                {
+                    Header = "Invalid configuration",
+                    Content = "Please verify that you provided correct list of project keys",
+                    ShowDuration = 2000
+                });
+            }
+        }
 
-      protected ImageSource GetImageFromResources(string path)
-      {
-         var uri = string.Format("pack://application:,,,/JiraAssistant;component/{0}", path.TrimStart('/'));
-         return _imageSourceConverter.ConvertFromString(uri) as ImageSource;
-      }
-   }
+        protected ImageSource GetImageFromResources(string path)
+        {
+            var uri = string.Format("pack://application:,,,/JiraAssistant;component/{0}", path.TrimStart('/'));
+            return _imageSourceConverter.ConvertFromString(uri) as ImageSource;
+        }
+    }
 }
--- JiraAssistant.Logic/Settings/AssistantSettings.cs
+++ JiraAssistant.Logic/Settings/AssistantSettings.cs
@@ -2,43 +2,43 @@
 
 namespace JiraAssistant.Logic.Settings
 {
-   public class AssistantSettings : SettingsBase
-   {
-      public string JiraUrl
-      {
-         get { return GetValue(defaultValue: string.Empty); }
+    public class AssistantSettings : SettingsBase
+    {
+        public string JiraUrl
+        {
+            get { return GetValue(defaultValue: string.Empty); }
 
-         set
-         {
-            if (value.StartsWith("http") == false)
-               value = "https://" + value;
+            set
+            {
+                if (value.StartsWith("http") == false)
+                    value = "https://" + value;
 
-            SetValue(value, defaultValue: string.Empty);
-         }
-      }
+                SetValue(value, defaultValue: string.Empty);
+            }
+        }
 
-      public string Username
-      {
-         get { return GetValue(defaultValue: string.Empty); }
-         set { SetValue(value, defaultValue: string.Empty); }
-      }
+        public string Username
+        {
+            get { return GetValue(defaultValue: string.Empty); }
+            set { SetValue(value, defaultValue: string.Empty); }
+        }
 
-      public string RecentBoardsIds
-      {
-         get { return GetValue(defaultValue: string.Empty); }
-         set { SetValue(value, defaultValue: string.Empty); }
-      }
+        public string RecentBoardsIds
+        {
+            get { return GetValue(defaultValue: string.Empty); }
+            set { SetValue(value, defaultValue: string.Empty); }
+        }
 
-      public DateTime LastLogin
-      {
-         get { return GetValue(defaultValue: DateTime.MinValue); }
-         set { SetValue(value, defaultValue: DateTime.MinValue); }
-      }
+        public DateTime LastLogin
+        {
+            get { return GetValue(defaultValue: DateTime.MinValue); }
+            set { SetValue(value, defaultValue: DateTime.MinValue); }
+        }
 
-      public string SessionCookies
-      {
-         get { return GetValue(defaultValue: string.Empty); }
-         set { SetValue(value, defaultValue: string.Empty); }
-      }
-   }
+        public string SessionCookies
+        {
+            get { return GetValue(defaultValue: string.Empty); }
+            set { SetValue(value, defaultValue: string.Empty); }
+        }
+    }
 }
