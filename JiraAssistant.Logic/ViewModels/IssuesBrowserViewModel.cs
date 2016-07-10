26c96d0b263e3b2061c150b82c7fab15907981e4
Adam Marszalek <adam.marszalek@gmail.com>
2016-07-10 22:35:38 +0200

    Restore issues list's filter load/save functionality.

--- JiraAssistant.Logic/ViewModels/IssuesBrowserViewModel.cs
+++ JiraAssistant.Logic/ViewModels/IssuesBrowserViewModel.cs
@@ -12,136 +12,140 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
+using Telerik.Windows.Controls;
 using Telerik.Windows.Data;
 using Telerik.Windows.Persistence;
+using Telerik.Windows.Persistence.Services;
 
 namespace JiraAssistant.Logic.ViewModels
 {
-    public class IssuesBrowserViewModel : ViewModelBase
-   {
-      private readonly string _settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                                                           "Yakuza", "Jira Assistant", "GridFilters");
-      private readonly IMessenger _messenger;
+    public class IssuesBrowserViewModel : GalaSoft.MvvmLight.ViewModelBase
+    {
+        private readonly string _settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                                                             "Yakuza", "Jira Assistant", "GridFilters");
+        private readonly IMessenger _messenger;
 
-      public RelayCommand SaveFiltersCommand { get; private set; }
-      public BindableRadGridView Grid { get; set; }
-      public RelayCommand LoadFiltersCommand { get; private set; }
-      public RelayCommand ExportToConfluenceCommand { get; private set; }
-      public RelayCommand PlainTextExportCommand { get; private set; }
-      public RelayCommand OpenScrumCardsCommand { get; private set; }
+        public RelayCommand SaveFiltersCommand { get; private set; }
+        public BindableRadGridView Grid { get; set; }
+        public RelayCommand LoadFiltersCommand { get; private set; }
+        public RelayCommand ExportToConfluenceCommand { get; private set; }
+        public RelayCommand PlainTextExportCommand { get; private set; }
+        public RelayCommand OpenScrumCardsCommand { get; private set; }
 
-      public QueryableCollectionView Issues { get; private set; }
-      public JiraIssue SelectedIssue { get; set; }
+        public QueryableCollectionView Issues { get; private set; }
+        public JiraIssue SelectedIssue { get; set; }
 
-      public IssuesBrowserViewModel(IList<JiraIssue> issues, IMessenger messenger)
-      {
-         Issues = new QueryableCollectionView(issues);
-         _messenger = messenger;
+        public IssuesBrowserViewModel(IList<JiraIssue> issues, IMessenger messenger)
+        {
+            Issues = new QueryableCollectionView(issues);
+            _messenger = messenger;
 
-         SaveFiltersCommand = new RelayCommand(SaveGridState);
-         LoadFiltersCommand = new RelayCommand(LoadGridState);
-         ExportToConfluenceCommand = new RelayCommand(ExportAsConfluenceMarkupResults);
-         PlainTextExportCommand = new RelayCommand(ExportAsTextResults);
-         OpenScrumCardsCommand = new RelayCommand(OpenScrumCards);
-      }
+            ServiceProvider.RegisterPersistenceProvider<ICustomPropertyProvider>(typeof(BindableRadGridView), new BindableGridViewPropertyProvider());
 
-      private void SaveGridState()
-      {
-         var dialog = new FilterNameDialog();
-         if (dialog.ShowDialog() == false)
-            return;
+            SaveFiltersCommand = new RelayCommand(SaveGridState);
+            LoadFiltersCommand = new RelayCommand(LoadGridState);
+            ExportToConfluenceCommand = new RelayCommand(ExportAsConfluenceMarkupResults);
+            PlainTextExportCommand = new RelayCommand(ExportAsTextResults);
+            OpenScrumCardsCommand = new RelayCommand(OpenScrumCards);
+        }
 
-         var name = Regex.Replace(dialog.FilterName, @"[^\w\s]", "_");
+        private void SaveGridState()
+        {
+            var dialog = new FilterNameDialog();
+            if (dialog.ShowDialog() == false)
+                return;
 
-         if (File.Exists(Path.Combine(_settingsPath, name)))
-         {
-            var result = MessageBox.Show("Do you want to overwrite existing filter?", "Jira Assistant", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            var name = Regex.Replace(dialog.FilterName, @"[^\w\s]", "_");
 
-            if (result == MessageBoxResult.No)
-               return;
-         }
+            if (File.Exists(Path.Combine(_settingsPath, name)))
+            {
+                var result = MessageBox.Show("Do you want to overwrite existing filter?", "Jira Assistant", MessageBoxButton.YesNo, MessageBoxImage.Warning);
 
-         var manager = new PersistenceManager();
-         manager.AllowCrossVersion = true;
-         var savedState = manager.Save(Grid);
-         using (var reader = new StreamReader(savedState))
-         using (var writer = new StreamWriter(Path.Combine(_settingsPath, name)))
-         {
-            writer.Write(reader.ReadToEnd());
-         }
-      }
+                if (result == MessageBoxResult.No)
+                    return;
+            }
 
-      public void PreviewSelectedIssue()
-      {
-         if (SelectedIssue != null)
-            _messenger.Send(new OpenIssueDetailsMessage(SelectedIssue));
-      }
+            var manager = new PersistenceManager();
+            manager.AllowCrossVersion = true;
+            var savedState = manager.Save(Grid);
+            using (var reader = new StreamReader(savedState))
+            using (var writer = new StreamWriter(Path.Combine(_settingsPath, name)))
+            {
+                writer.Write(reader.ReadToEnd());
+            }
+        }
 
-      private void LoadGridState()
-      {
-         var filters = Directory.EnumerateFiles(_settingsPath).Select(p => Path.GetFileName(p)).ToArray();
-         var dialog = new SelectFilterDialog(filters);
-         if (dialog.ShowDialog() == false)
-            return;
+        public void PreviewSelectedIssue()
+        {
+            if (SelectedIssue != null)
+                _messenger.Send(new OpenIssueDetailsMessage(SelectedIssue));
+        }
 
-         var manager = new PersistenceManager();
-         manager.AllowCrossVersion = true;
-         using (var stream = File.OpenRead(Path.Combine(_settingsPath, dialog.FilterName)))
-         {
-            manager.Load(Grid, stream);
-         }
-      }
+        private void LoadGridState()
+        {
+            var filters = Directory.EnumerateFiles(_settingsPath).Select(p => Path.GetFileName(p)).ToArray();
+            var dialog = new SelectFilterDialog(filters);
+            if (dialog.ShowDialog() == false)
+                return;
 
-      private void ExportAsConfluenceMarkupResults()
-      {
-         var resultBuilder = new StringBuilder();
-         var grouped = Issues.OfType<JiraIssue>().GroupBy(i => i.EpicName);
+            var manager = new PersistenceManager();
+            manager.AllowCrossVersion = true;
+            using (var stream = File.OpenRead(Path.Combine(_settingsPath, dialog.FilterName)))
+            {
+                manager.Load(Grid as RadGridView, stream);
+            }
+        }
 
-         foreach (var group in grouped)
-         {
-            resultBuilder.AppendLine();
-            if (string.IsNullOrWhiteSpace(group.Key))
-               resultBuilder.AppendLine("h2. (No Epic)");
-            else
-               resultBuilder.AppendLine("h2. " + group.Key);
-            resultBuilder.AppendLine();
+        private void ExportAsConfluenceMarkupResults()
+        {
+            var resultBuilder = new StringBuilder();
+            var grouped = Issues.OfType<JiraIssue>().GroupBy(i => i.EpicName);
 
-            foreach (var issue in group)
-               resultBuilder.AppendLine(string.Format("* *{0}* - {1}", issue.Key, EscapeConfluenceMarkupCharacters(issue.Summary)));
-         }
+            foreach (var group in grouped)
+            {
+                resultBuilder.AppendLine();
+                if (string.IsNullOrWhiteSpace(group.Key))
+                    resultBuilder.AppendLine("h2. (No Epic)");
+                else
+                    resultBuilder.AppendLine("h2. " + group.Key);
+                resultBuilder.AppendLine();
 
-         var dialog = new TextualPreview(resultBuilder.ToString());
-         dialog.ShowDialog();
-      }
+                foreach (var issue in group)
+                    resultBuilder.AppendLine(string.Format("* *{0}* - {1}", issue.Key, EscapeConfluenceMarkupCharacters(issue.Summary)));
+            }
 
-      private string EscapeConfluenceMarkupCharacters(string summary)
-      {
-         return Regex.Replace(summary, @"[{\[\]\}\(\)!@\\]", m => "\\" + m.Value);
-      }
+            var dialog = new TextualPreview(resultBuilder.ToString());
+            dialog.ShowDialog();
+        }
 
-      private void ExportAsTextResults()
-      {
-         var resultBuilder = new StringBuilder();
-         var grouped = Issues.OfType<JiraIssue>().GroupBy(i => i.EpicName);
+        private string EscapeConfluenceMarkupCharacters(string summary)
+        {
+            return Regex.Replace(summary, @"[{\[\]\}\(\)!@\\]", m => "\\" + m.Value);
+        }
 
-         foreach (var group in grouped)
-         {
-            if (string.IsNullOrWhiteSpace(group.Key))
-               resultBuilder.AppendLine("(No Epic)");
-            else
-               resultBuilder.AppendLine(group.Key);
+        private void ExportAsTextResults()
+        {
+            var resultBuilder = new StringBuilder();
+            var grouped = Issues.OfType<JiraIssue>().GroupBy(i => i.EpicName);
 
-            foreach (var issue in group)
-               resultBuilder.AppendLine(string.Format("* {0} - {1}", issue.Key, issue.Summary));
-         }
+            foreach (var group in grouped)
+            {
+                if (string.IsNullOrWhiteSpace(group.Key))
+                    resultBuilder.AppendLine("(No Epic)");
+                else
+                    resultBuilder.AppendLine(group.Key);
 
-         var dialog = new TextualPreview(resultBuilder.ToString());
-         dialog.ShowDialog();
-      }
+                foreach (var issue in group)
+                    resultBuilder.AppendLine(string.Format("* {0} - {1}", issue.Key, issue.Summary));
+            }
 
-      private void OpenScrumCards()
-      {
-         _messenger.Send(new OpenScrumCardsMessage(Issues.OfType<JiraIssue>().ToList()));
-      }
-   }
+            var dialog = new TextualPreview(resultBuilder.ToString());
+            dialog.ShowDialog();
+        }
+
+        private void OpenScrumCards()
+        {
+            _messenger.Send(new OpenScrumCardsMessage(Issues.OfType<JiraIssue>().ToList()));
+        }
+    }
 }
