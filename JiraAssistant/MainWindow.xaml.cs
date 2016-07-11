6f42c9d1a3a7375556cc81d8e287bd0e0e7faf2f
Adam Marszalek <adam.marszalek@gmail.com>
2016-07-11 23:00:04 +0200

    Added support for "Back" button on mouse to navigate to previous screen.

--- JiraAssistant/MainWindow.xaml.cs
+++ JiraAssistant/MainWindow.xaml.cs
@@ -1,23 +1,32 @@
 ﻿using JiraAssistant.Pages;
 using JiraAssistant.Logic.ContextlessViewModels;
+using System.Windows.Input;
 
 namespace JiraAssistant
 {
-   public partial class MainWindow
-   {
-      public MainWindow()
-      {
-         InitializeComponent();
-         Loaded += (sender, args) =>
-         {
-            var viewModel = DataContext as MainViewModel;
-            viewModel.NavigateTo(new LoginPage());
-         };
-         Closing += (sender, args) =>
-         {
-            var viewModel = DataContext as MainViewModel;
-            viewModel.HandleClosing(args);
-         };
-      }
-   }
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += (sender, args) =>
+            {
+                var viewModel = DataContext as MainViewModel;
+                viewModel.NavigateTo(new LoginPage());
+            };
+            Closing += (sender, args) =>
+            {
+                var viewModel = DataContext as MainViewModel;
+                viewModel.HandleClosing(args);
+            };
+            MouseDown += (sender, args) =>
+            {
+                if (args.ChangedButton != MouseButton.XButton1)
+                    return;
+
+                var viewModel = DataContext as MainViewModel;
+                viewModel.Back();
+            };
+        }
+    }
 }
