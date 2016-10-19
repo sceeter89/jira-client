ff165fd8e1f68819d55e98d41f2c5f672d08085c
Adam Marszałek <amarszalek@ganymede.eu>
2016-10-19 22:28:27 +0200

    Strip last part of version number on update window.

---
+++ JiraAssistant.Controls/Converters/VersionToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace JiraAssistant.Controls.Converters
+{
+    public class VersionToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int partsCount;
+            try
+            {
+                partsCount = System.Convert.ToInt32(parameter);
+            }
+            catch
+            {
+                partsCount = 4;
+            }
+
+            if (value is Version == false)
+                return null;
+
+            return ((Version)value).ToString(partsCount);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
